0eec2b69241ede44e62ab4823a1de8a86bdae7eb
Unknown <p.Soriot@rubika-edu.com>
2018-10-24 16:22:23 +0200

    RecupDash + bounce

--- TestAndroidJeuMobile/Assets/Script/PlayerDash.cs
+++ TestAndroidJeuMobile/Assets/Script/PlayerDash.cs
@@ -5,46 +5,50 @@ using UnityEngine;
 
 public class PlayerDash : MonoBehaviour
 {
+
+    [Header ("anim")]
     public Animator anim;
-    public int cooldownDash;
-    public float forceGravite;
+
+    [Header("Twic dash")]
+    public float cooldownDash;
     public Rigidbody body;
+    public float dashSpeed;
+
+
+    // Recup vecteur de dash
+    float lengthSaut;
     Vector2 posDepart;
     Vector2 direction;
-    float lengthSaut;
-    bool directionChosen=false;
+
+    //check possibilité de dash/declenchement dash
+    bool directionChosen = false;
+    bool canDash = true;
+
 
 
     void Update()
     {
+        if (canDash==true)
+            RecupVector();
 
+       // CheckBump();
 
-        if (directionChosen == false)
-        {
-            recupVector();
-        }
-        else
-        {
-            coolDownDash();
-        }
     }
 
 
 
     private void FixedUpdate()
     {
-        Gravity();
-        CheckSaut();
+
+
+       // CheckDash();
     }
 
 
 
-    void Dash()
-    {
-        body.velocity = -direction * lengthSaut;
-    }
 
-    void recupVector()
+
+    void RecupVector()
     {
         if (Input.touchCount > 0)
         {
@@ -54,25 +58,40 @@ public class PlayerDash : MonoBehaviour
 
             Touch touch = Input.GetTouch(0);
 
-
             switch (touch.phase)
             {
                 case TouchPhase.Began:
                     {
+                        anim.SetBool("prepDash", true);
+                        Time.timeScale = 0.2f;
                         posDepart = Input.GetTouch(0).position;
                         break;
                     }
                 case TouchPhase.Moved:
                     {
-                        lengthSaut = Vector2.Distance(touch.position, posDepart);
+                        Time.timeScale = 0.2f;
                         direction = touch.position - posDepart;
+                        lengthSaut = Vector2.Distance(touch.position, posDepart);
+                        if (lengthSaut > 45 )
+                            lengthSaut = 45;
+                    
                         break;
                     }
                 case TouchPhase.Ended:
                     {
+                        body.velocity = new Vector3(0, 0, 0);
+
+
+                        print(lengthSaut);
+                        Time.timeScale = 1f;
+
+                        anim.SetBool("prepDash", false);
                         anim.SetBool("dashing", true);
                         directionChosen = true;
+                        StartCoroutine("coolDownDash");
+                        canDash = false;
                         print(direction.normalized);
+               
                         break;
                     }
             }
@@ -80,24 +99,38 @@ public class PlayerDash : MonoBehaviour
 
     }
 
-    void Gravity()
+    void Dash()
     {
-        body.velocity += new Vector3(0, forceGravite, 0);
-        if (body.velocity.y < -10)
-            body.velocity = new Vector3(body.velocity.x, -10, 0);
+        //  body.velocity = -direction * lengthSaut/6*(dashSpeed/100);
+        body.AddForce(-direction * lengthSaut / 18);
+        print(body.velocity);
+
     }
 
-    void CheckSaut()
+    void DirectDash()
     {
-        if (body.velocity.y<=0)
-        {
-            anim.SetBool("dashing", false);
-        }
+       //body     
     }
+
 
     IEnumerator coolDownDash()
     {
         yield return new WaitForSeconds(cooldownDash);
-        directionChosen = true;
+        canDash = true;
     }
+
+    void Gravity()
+    {/*
+        body.velocity += new Vector3(0, forceGravite, 0);
+        if (body.velocity.y < -10)
+            body.velocity = new Vector3(body.velocity.x, -10, 0);
+
+        if (body.velocity.x>0)
+            body.velocity = new Vector3(body.velocity.x - 0.02f, body.velocity.y, 0);
+        else if (body.velocity.x<0)
+            body.velocity = new Vector3(body.velocity.x + 0.02f, body.velocity.y, 0);*/
+    }
+
+
+
 }
